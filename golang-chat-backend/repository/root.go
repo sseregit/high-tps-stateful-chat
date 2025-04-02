66bf21dfd43af94d9592c24b32876c3506db5fda
장연식 <sseregit@knowwheresoft.com>
2025-04-02 16:05:53 +0900

    MySQL에 쿼리를 작성할껀데, ORM은 사용해야할까요?

--- golang-chat-backend/repository/root.go
+++ golang-chat-backend/repository/root.go
@@ -3,13 +3,21 @@ package repository
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"strings"
 	"websocket-high-tps-chat/config"
+	"websocket-high-tps-chat/types/schema"
 )
 
 type Repository struct {
 	cfg *config.Config
 	db  *sql.DB
 }
+
+const (
+	room       = "chatting.room"
+	chat       = "chatting.chat"
+	serverInfo = "chatting.serverInfo"
+)
 
 func NewRepository(cfg *config.Config) (*Repository, error) {
 	r := &Repository{cfg: cfg}
@@ -20,4 +28,22 @@ func NewRepository(cfg *config.Config) (*Repository, error) {
 	} else {
 		return r, nil
 	}
+}
+
+func (s *Repository) Room(name string) (*schema.Room, error) {
+	d := new(schema.Room)
+	qs := query([]string{"SELECT * FROM", room, "WHERE name = ?"})
+
+	err := s.db.QueryRow(qs, name).Scan(
+		&d.ID,
+		&d.Name,
+		&d.CreateAt,
+		&d.UpdateAt,
+	)
+
+	return d, err
+}
+
+func query(qs []string) string {
+	return strings.Join(qs, " ") + ";"
 }
