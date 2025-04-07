2fece816d52ad33bca88dfe8674c2474576e029b
장연식 <sseregit@knowwheresoft.com>
2025-04-07 15:52:55 +0900

    kafka를 구동시키고, terminal를 통해서 Pub/Sub이 먼지 실습해볼게요.

---
+++ sql/init.sql
@@ -0,0 +1,21 @@
+USE chatting;
+
+CREATE TABLE IF NOT EXISTS room (
+    `id` bigint unsigned primary key NOT NULL auto_increment,
+    `name` varchar(255) not null  unique,
+    `createAt` datetime default current_timestamp,
+    `updateAt` datetime default current_timestamp on update current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS chat (
+    `id` bigint unsigned primary key NOT NULL auto_increment,
+    `room` varchar(255) not null,
+    `name` varchar(255) not null,
+    `message` varchar(255) not null,
+    `when` datetime default current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS serverInfo (
+    `ip` varchar(255) primary key NOT NULL,
+    `avaliable` bool not null
+);
